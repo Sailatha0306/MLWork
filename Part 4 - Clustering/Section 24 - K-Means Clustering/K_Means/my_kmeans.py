8c4643abc51ca8e675a403c698fec3d5ea9fac0e
rtamir <rk4ravikiran@gmail.com>
2018-02-06 14:47:14 +0530

    latest

--- Part 4 - Clustering/Section 24 - K-Means Clustering/K_Means/my_kmeans.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/K_Means/my_kmeans.py
@@ -5,3 +5,16 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
+
+#elbow method
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=1,init = 'k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
