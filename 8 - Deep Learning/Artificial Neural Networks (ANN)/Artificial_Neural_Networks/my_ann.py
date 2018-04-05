1b1490c7d05bcbf09098f561d8f3967f41b845a4
rtamir <rk4ravikiran@gmail.com>
2018-04-05 19:41:42 +0530

    commit

--- 8 - Deep Learning/Artificial Neural Networks (ANN)/Artificial_Neural_Networks/my_ann.py
+++ 8 - Deep Learning/Artificial Neural Networks (ANN)/Artificial_Neural_Networks/my_ann.py
@@ -11,8 +11,7 @@
 
 # Part 1 - Data Preprocessing
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
+
 import pandas as pd
 
 # Importing the dataset
--- Datasets/facerecognition/video_rec_webcam.py
+++ Datasets/facerecognition/video_rec_webcam.py
@@ -1,11 +1,17 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,960)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,540)
+
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
-    cv2.waitKey(10)
+    ret, framex = cap.read()
+    
+    frame = cv2.resize(framex,(960,540))
+    
+    cv2.waitKey(30)
     
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
