e22362b4673c3ea17b3ff73602d5582ff509296c
rtamir <rk4ravikiran@gmail.com>
2018-08-05 16:17:05 +0530

    c

---
+++ 04 - Clustering/K-Means Clustering/documentclustering/untitled0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 27 16:43:55 2018
+
+@author: Ravikiran.Tamiri
+"""
+
+import nltk
+import re
+import os
+import codecs
+from sklearn import feature_extraction
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+print("Hello")
